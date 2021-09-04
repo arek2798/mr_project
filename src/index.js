3c509032507a94ec6871f9fe8535c23ef2ad9cc7
Arkadiusz Kuptz <arekk2798@outlook.com>
2021-09-04 22:19:02 +0200

    register service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RootView from './views/Root/Root';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <RootView />,
@@ -12,3 +13,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
